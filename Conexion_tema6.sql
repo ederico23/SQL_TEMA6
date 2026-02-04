f27c8ef70dd348eb08f6126be196fc6848369cc9
eder23 <edergracia006@gmail.com>
2026-02-04 21:00:16 +0100

    first

--- Conexion_tema6.sql
+++ Conexion_tema6.sql
@@ -1,10 +1,17 @@
 -- creamos la tabla --
-
 CREATE TABLE PROVINCIAS
 (   
  --nombre de la variable.  tipo de dato.  contraint nombreTabla_tipoDeId_tipoClave--
     Cod_provi number(2) constraint PROVINCIAS_COD_PK primary key,
     Nombre varchar(25) constraint PROVINCIAS_NOM_NN not null,
-    Pais varchar2(25) constraint PROVINCIAS_PAIS_CH check (Pais in("España" , "Portugal", "Italia"))
-    
-)     
+    Pais varchar2(25),
+    constraint PROVINCIAS_PAIS_CH check (Pais in( 'España' , 'Portugal', 'Italia'))
+);     
+
+
+insert into PROVINCIAS (Cod_provi, Nombre, Pais)
+values (1, 'Avila' , 'España');
+select * from PROVINCIAS;
+
+
+
